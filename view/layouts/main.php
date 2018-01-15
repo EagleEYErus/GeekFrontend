df1e7d38db1873cf818467c51fc8b17f56fe3853
Alexander Saprykin <alex_afsoc@mail.ru>
2018-01-15 22:43:06 +0300

    Added missing scripts

--- view/layouts/main.php
+++ view/layouts/main.php
@@ -137,6 +137,14 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
         integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
         crossorigin="anonymous"></script>
+<?php
+if (\components\RequestComponent::$controller === 'product') {
+    echo "<script src='http://propeller.in/components/range-slider/js/wNumb.js'></script>
+          <script src='http://propeller.in/components/range-slider/js/nouislider.js'></script>
+          <script src='../../web/js/product.js'></script>";
+}
+
+?>
 </body>
 
 </html>
