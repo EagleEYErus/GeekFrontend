b6f9961d9b9060681b3ccfd7da1938ab2b3df337
Alexander Saprykin <alex_afsoc@mail.ru>
2018-01-16 17:11:55 +0300

    Removed unused package

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const htmlbeautify = require('gulp-html-beautify');
 const plumber = require('gulp-plumber');
 const watch = require('gulp-watch');
 
-gulp.task('scss', () => {
-	const run = () => {
-		return gulp
-			.src(['./scss/*.scss', '!./scss/header.scss', '!./scss/footer.scss'])
-			.pipe(plumber())
-			.pipe(sass())
-			.pipe(gulp.dest('./css'));
-	}
-	watch('./scss/*.scss', run);
+gulp.task('scss', function () {
+    function run() {
+        return gulp
+            .src(['./scss/*.scss', '!./scss/header.scss', '!./scss/footer.scss'])
+            .pipe(plumber())
+            .pipe(sass())
+            .pipe(gulp.dest('./css'));
+    }
 
-	return run();
+    watch('./scss/*.scss', run);
+
+    return run();
 });
 
 gulp.task('default', ['scss']);
